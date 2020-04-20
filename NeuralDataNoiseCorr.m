544fe252fe10c85ea4209a05381e00c2182dc369
SohrabSalimian <42098944+SohrabSalimian@users.noreply.github.com>
2020-04-20 14:37:55 -0500

    WaterGrabSegmentation Functions
    Functions for plotting noise correlations for the watergrab data.

---
+++ NeuralDataNoiseCorr.m
@@ -0,0 +1,44 @@
+function [NoiseCorrelation, NoiseResp, RoiKeep ]...
+    = NeuralDataNoiseCorr(EventLockedDat, TimeLength, NumRois, LR, neuropil)
+%NeuralDataNoiseCorr takes in as input the eventLockedData, length of time
+%and number of Rois. It returns the noise correlation matrix across all
+%trials as well as the noise response matrix for the left and right trials.
+
+
+EvntLockedMat = [EventLockedDat(:).data];
+EvntLockedMat = reshape(EvntLockedMat, length(TimeLength), NumRois, []);
+
+Left = find(LR ==1 );
+Right = find(LR == 2);
+RoiSums = sum(EvntLockedMat, [1,3]);
+
+if neuropil == 1
+    thresh = median(RoiSums);
+else
+    thresh = median(RoiSums)/2;
+    
+end
+
+RoiRemoved = (RoiSums < thresh);
+[~, RoiKeep] = find(RoiRemoved == 0);
+EvntLockedMat = EvntLockedMat(:, RoiKeep, :);
+
+
+RightTrials = EvntLockedMat(:, :, Right);
+LeftTrials = EvntLockedMat(:, :, Left);
+RightTrialAvg = mean(RightTrials, [1, 3]); 
+LeftTrialAvg = mean(LeftTrials, [1, 3]);
+
+RightTrialFR = squeeze(mean(RightTrials, 1))';
+LeftTrialFR = squeeze(mean(LeftTrials, 1))';
+
+
+RightNoiseResp = reshape(RightTrialFR - RightTrialAvg, [], length(RoiKeep));
+RightNoiseResp(RightNoiseResp < 0) = 0;
+LeftNoiseResp = reshape(LeftTrialFR - LeftTrialAvg, [], length(RoiKeep));
+LeftNoiseResp(LeftNoiseResp < 0) = 0;
+
+NoiseResp = cat(1, RightNoiseResp, LeftNoiseResp);
+NoiseCorrelation = corrcoef(NoiseResp);
+end
+
---
+++ NeuroDistCorr.m
@@ -0,0 +1,36 @@
+function [MeanCorrDist, Edges, RoiDist] = NeuroDistCorr(NoiseCorr, RoiLocations, SigRois, BinWidth)
+%NeuroPilDistCorr
+%Takes as input a noise correlation matrix, roi locations,significant rois, 
+%BinWidth and returns a bar plot of the noise correlations binned by distance. 
+
+RoiLocations = RoiLocations(SigRois, :);
+
+
+
+RoiDist = zeros(length(SigRois), length(SigRois));
+for i=1:length(RoiLocations)
+    RoiDist(:, i) = sqrt(sum((RoiLocations(i, :) - ...
+        RoiLocations(:, :)).^2, 2));
+    
+end
+
+NanVec = NaN(1, length(SigRois));
+RoiDist = RoiDist - diag(diag(RoiDist)) + diag(NanVec);
+MaxDist = max(RoiDist, [], 'all');
+MinDist = min(RoiDist, [], 'all');
+
+NoiseCorrDist = NoiseCorr - diag(diag(NoiseCorr));
+
+Edges = MinDist:BinWidth:MaxDist+100;
+
+[Allocate, ~] = discretize(RoiDist, Edges);
+
+MeanCorrDist = zeros(length(Edges), 1);
+for i=1:length(Edges)
+    indices = Allocate == i;
+    MeanCorrDist(i, 1) = nanmean(NoiseCorrDist(indices));   
+end
+
+
+end
+
